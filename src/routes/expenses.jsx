6b4bcef4d74abd1da1c3672f3176d041ae4a719d
will <will@xiaohongshu.com>
2022-08-11 00:08:09 +0800

    feat: redux mini demo

--- src/routes/expenses.jsx
+++ src/routes/expenses.jsx
@@ -1,7 +1,14 @@
+import globalStore from '../states/index'
+import { useState, useEffect } from 'react'
+
 export default function Expenses() {
+  const [state, setState] = useState(globalStore.getState().value);
+  useEffect(() => {
+    globalStore.subscribe(() => setState(globalStore.getState().value))
+  }, [])
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Expenses</h2>
+      <h2>Expenses { state }</h2>
     </main>
   );
 }
--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,7 +1,19 @@
+import globalStore from '../states/index'
+import { useState,useEffect } from 'react'
+
 export default function Invoices() {
+  const [state, setState] = useState(globalStore.getState().value);
+  useEffect(() => { 
+    globalStore.subscribe(() => setState(globalStore.getState().value))
+  },[])
+  
+
+
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
+      <h2>Invoices {state}</h2>
+      <button onClick={() => globalStore.dispatch({ type: 'counter/incremented' })}>加1</button>
+      <button onClick={() => globalStore.dispatch({ type: 'counter/decremented' })}>减1</button>
     </main>
   );
 }
